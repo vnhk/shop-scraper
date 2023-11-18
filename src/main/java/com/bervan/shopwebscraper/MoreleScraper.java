c0336e07c5e97ef5ac97ed06ad24f9d22518c191
berluk12@gmail.com <berluk12@gmail.com>
2023-11-18 23:54:39 +0100

    scrap update

---
+++ src/main/java/com/bervan/shopwebscraper/MoreleScraper.java
@@ -0,0 +1,110 @@
+package com.bervan.shopwebscraper;
+
+import com.bervan.shopwebscraper.save.ExcelService;
+import com.bervan.shopwebscraper.save.JsonService;
+import com.bervan.shopwebscraper.save.StatServerService;
+import org.apache.logging.log4j.util.Strings;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.WebDriver;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("Morele")
+public class MoreleScraper extends Scraper {
+
+    public MoreleScraper(JsonService jsonService, ExcelService excelService, StatServerService statServerService) {
+        super(jsonService, excelService, statServerService);
+    }
+
+    @Override
+    protected int getNThreadsForConcurrentProcessing() {
+        return 8;
+    }
+
+    @Override
+    protected String getFirstPageUrlWithParams(String url) {
+        return url;
+    }
+
+    @Override
+    protected int getNumberOfPages(WebDriver driver) {
+        String pageSource = driver.getPageSource();
+        Document parsed = Jsoup.parse(pageSource);
+        Elements paginationLastPage = parsed.getElementsByClass("pagination-btn-nolink-anchor");
+        String pages = "1";
+        try {
+            if (paginationLastPage.size() == 0) {
+                pages = parsed.select(".pagination.dynamic").attr("data-count").trim();
+            } else {
+                pages = paginationLastPage.get(0).text().trim();
+            }
+        } catch (Exception e) {
+            System.err.println("Could not find pages! Only one page will be processed!");
+            pages = "1";
+        }
+        return Integer.parseInt(pages);
+    }
+
+    @Override
+    protected List<Element> loadAllOffersTiles(WebDriver driver) {
+        Document doc = Jsoup.parse(driver.getPageSource());
+        return doc.getElementsByClass("cat-product");
+    }
+
+    @Override
+    protected String getUrlWithParametersForPage(String url, int currentPage) {
+        if (url.contains(",,,,")) {
+            return url + "/" + currentPage;
+        }
+        return url + "/,,,,,,,,0,,,,/" + currentPage;
+    }
+
+    @Override
+    protected void processProductAdditionalAttributes(Element offerElement, Offer offer) {
+        Element attributes = offerElement.select(".cat-product-features").first();
+        if (attributes != null) {
+            List<Element> items = attributes.select(".cat-product-feature");
+            for (Element item : items) {
+                String attributeName = sanitize(item.html()
+                        .split("<b>")[0]
+                        .trim());
+                if (attributeName.endsWith(":")) {
+                    attributeName = attributeName.substring(0, attributeName.length() - 1);
+                }
+                String attributeValues = item.attr("title").trim();
+                if (!attributeName.isBlank()) {
+                    offer.put(attributeName, Arrays.stream(attributeValues.split(", "))
+                            .map(String::trim)
+                            .map(this::sanitize)
+                            .filter(Strings::isNotEmpty)
+                            .collect(Collectors.toList()));
+                }
+            }
+        }
+    }
+
+    @Override
+    protected String getOfferPrice(Element offer) {
+        return offer.select(".price-new")
+                .text()
+                .split("zł")[0]
+                .split(",")[0]
+                .trim();
+    }
+
+    @Override
+    protected String getOfferHref(Element offer) {
+        return getFirstIfFoundAttrByCssQuery(offer, "a.productLink", "href");
+    }
+
+    @Override
+    protected String getOfferName(Element offer) {
+        return getFirstIfFoundTextByCssQuery(offer, "a.productLink");
+    }
+}
--- src/main/java/com/bervan/shopwebscraper/ScrapProcessor.java
+++ src/main/java/com/bervan/shopwebscraper/ScrapProcessor.java
@@ -42,7 +42,7 @@ public class ScrapProcessor {
 
         for (Future task : tasks) {
             try {
-                task.get(120, TimeUnit.MINUTES);
+                task.get(20, TimeUnit.MINUTES);
             } catch (Exception e) {
                 e.printStackTrace();
             }
