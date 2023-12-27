eccc3b61d1c8a85f8d3e03128c70a665ef13284d
berluk12@gmail.com <berluk12@gmail.com>
2023-12-27 13:15:13 +0100

    added refreshing views

--- src/main/java/com/bervan/shopwebscraper/ShopWebscraperApplication.java
+++ src/main/java/com/bervan/shopwebscraper/ShopWebscraperApplication.java
@@ -2,7 +2,13 @@ package com.bervan.shopwebscraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +18,11 @@ public class ShopWebscraperApplication {
         SpringApplication.run(ShopWebscraperApplication.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.of(1, ChronoUnit.MINUTES))
+                .setReadTimeout(Duration.of(1, ChronoUnit.HOURS))
+                .build();
+    }
 }
