bfb984ba960d649035a7c8a8c0c07b1aaa0e535a
berluk12@gmail.com <berluk12@gmail.com>
2024-01-29 21:39:51 +0100

    Scheduler

--- src/main/java/com/bervan/shopwebscraper/scheduler/SchedulerTasks.java
+++ src/main/java/com/bervan/shopwebscraper/scheduler/SchedulerTasks.java
@@ -21,26 +21,8 @@ public class SchedulerTasks {
         this.statServerService = statServerService;
     }
 
-    @Scheduled(cron = "0 0 8 * * *")
+    @Scheduled(cron = "0 0 6 * * *")
     public void refreshView1() {
-        try {
-            statServerService.refreshViews();
-        } catch (Exception e) {
-            log.error("RefreshingViews: FAILED!", e);
-        }
-    }
-
-    @Scheduled(cron = "0 10 15 * * *")
-    public void refreshView2() {
-        try {
-            statServerService.refreshViews();
-        } catch (Exception e) {
-            log.error("RefreshingViews: FAILED!", e);
-        }
-    }
-
-    @Scheduled(cron = "0 0 19 * * *")
-    public void refreshView3() {
         try {
             statServerService.refreshViews();
         } catch (Exception e) {
@@ -51,14 +33,14 @@ public class SchedulerTasks {
     @Scheduled(cron = "0 0 * * * *")
     public void scrapRTVEuroAGD() throws InterruptedException {
         Thread.sleep(RandomUtil.getPositiveInt() % 15000);
-        log.info("RTV Euro AGD Scraping: STARTED!");
+        log.info("Scraping: STARTED!");
         try {
             LocalDateTime now = LocalDateTime.now();
             scrapProcessor.run(true, "config.json", now.getHour(), "RTV Euro AGD", "Morele", "Media Expert");
             statServerService.refreshViews();
-            log.info("RTV Euro AGD Scraping: COMPLETED!");
+            log.info("Scraping: COMPLETED!");
         } catch (Exception e) {
-            log.error("RTV Euro AGD Scraping: FAILED!", e);
+            log.error("Scraping: FAILED!", e);
         }
     }
 }
