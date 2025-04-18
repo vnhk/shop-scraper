ac832ba0771d0c2c2e612da8b4339454a58d8659
berluk12@gmail.com <berluk12@gmail.com>
2025-04-18 12:33:11 +0200

    Queue

--- src/main/java/com/bervan/shopwebscraper/CustomRestTemplateConfiguration.java
+++ src/main/java/com/bervan/shopwebscraper/CustomRestTemplateConfiguration.java
@@ -25,23 +25,23 @@ import java.security.cert.CertificateException;
 @Configuration
 public class CustomRestTemplateConfiguration {
 
-    @Value("${trust.store}")
-    private Resource trustStore;
+//    @Value("${trust.store}")
+//    private Resource trustStore;
+//
+//    @Value("${trust.store.password}")
+//    private String trustStorePassword;
 
-    @Value("${trust.store.password}")
-    private String trustStorePassword;
-
-    @Bean
-    public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, MalformedURLException, IOException {
-
-        SSLContext sslContext = new SSLContextBuilder()
-                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
-        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
-        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
-                .setSSLSocketFactory(sslConFactory)
-                .build();
-        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
-        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return new RestTemplate(requestFactory);
-    }
+//    @Bean
+//    public RestTemplate restTemplate() throws KeyManagementException, NoSuchAlgorithmException, KeyStoreException, CertificateException, MalformedURLException, IOException {
+//
+//        SSLContext sslContext = new SSLContextBuilder()
+//                .loadTrustMaterial(trustStore.getURL(), trustStorePassword.toCharArray()).build();
+//        SSLConnectionSocketFactory sslConFactory = new SSLConnectionSocketFactory(sslContext);
+//        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+//                .setSSLSocketFactory(sslConFactory)
+//                .build();
+//        CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(cm).build();
+//        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//        return new RestTemplate(requestFactory);
+//    }
 }
